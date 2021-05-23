20cfacf6810346ddaf59db8513d44d083f67c446
Hilary Richardson <hilary.richardson@gmail.com>
2021-05-23 10:57:16 -0700

    adding webpack bootstrap and popper js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require("webpack");
+require("bootstrap");
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -6,5 +8,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
     mode: 'development'
 };
